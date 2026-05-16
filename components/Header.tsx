43c5fb72ca57f1a479845da8362f85d25cf1d62c
Simon <simon.ropiot44@gmail.com>
2026-05-16 16:07:51 +0200

    new:

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,6 +47,7 @@ export default function Header() {
               <span className="hidden sm:inline">Réserver une démo</span>
             </CtaButton>
           </div>
+        </div>
       </div>
     </header>
   );
